7d999078ecc3bd984f15e8ef95493ee46bca4552
ZimmermannnnamremmiZ <Maximvo92@gmail.com>
2022-05-06 01:14:45 +0300

    adaptive

--- src/pages/Hotels/index.js
+++ src/pages/Hotels/index.js
@@ -1,4 +1,5 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { useMediaQuery } from 'react-responsive'
 
 import FindHotelsForm from "../../components/forms/findHotelsForm/FindHotelsForm";
 import Favorite from "../../components/favorite/Favorite";
@@ -11,6 +12,19 @@ const Hotels = ({setToken}) => {
     const [formVisible, setFormVisible] = useState()
     const [favVisible, setFavVisible] = useState()
 
+
+    const isTablet = useMediaQuery({ minWidth: 768})
+
+    useEffect(() => {
+        function handleResize() {
+          return isTablet ? setFormVisible('visible') : setFormVisible('hidden')
+        }
+
+        window.addEventListener('resize', handleResize)
+      })
+
+
+
     return(
         <div className="hotels">
             <Header setToken={setToken}/>
@@ -20,10 +34,12 @@ const Hotels = ({setToken}) => {
                         <button className="tablet__fav-btn" onClick={() => setFavVisible('visible')}/>
                 </div>
                 <div className="left-block flex-col">
-                    <div className="form-block" style={{visibility: favVisible}}>
+                    <div className="form-block" style={{visibility: formVisible}}>
+                        <button className="close" onClick={() => setFormVisible('hidden')} />
                         <FindHotelsForm />
                     </div>
-                    <div className="favorite-block" style={{visibility: formVisible}}>
+                    <div className="favorite-block" style={{visibility: favVisible}}>
+                        <button className="close" onClick={() => setFavVisible('hidden')} />
                         <Favorite />
                     </div>
                 </div>
